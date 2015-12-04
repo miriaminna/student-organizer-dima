e033d5ebae4693f65c937c447cdf0edf59ca7d72
Dmytro Berezin <berezindev@gmail.com>
2015-12-04 13:49:07 +0200

    DB

--- app/src/main/java/app/studentorganizer/db/Database.java
+++ app/src/main/java/app/studentorganizer/db/Database.java
@@ -16,7 +16,6 @@ public class Database extends SQLiteOpenHelper {
     private static final String SIMPLE_TO_MULTI = "simple_to_multi";
     private static final String TEACHERS = "teachers";
     private static final String SUBJECTS = "subjects";
-    private static final String TEACHER_TO_SUBJECT = "teacher_to_subject";
     private static final String CONTENTS = "content";
     private static final String CONTENT_ITEMS = "content_items";
     private static final String TESTS = "tests";
@@ -28,16 +27,14 @@ public class Database extends SQLiteOpenHelper {
     public static final String SIMPLE_TASK_ID = "_id";
     public static final String SIMPLE_TASK_NAME = "_name";
     public static final String SIMPLE_TASK_SUBJECT_ID = "_subject_id";
+    public static final String SIMPLE_TASK_MULTI_TASK_ID = "_multi_task_id";
     public static final String SIMPLE_TASK_DEADLINE = "_deadline";
     public static final String SIMPLE_TASK_POINTS = "_points";
 
     // MULTI_TASKS column names
     public static final String MULTI_TASK_ID = "_id";
     public static final String MULTI_TASK_NAME = "_name";
-
-    // SIMPLE_TO_MULTI column names
-    public static final String SIMPLE_TO_MULTI_SIMPLE_TASK_ID = "_simple_task_id";
-    public static final String SIMPLE_TO_MULTI_MULTI_TASK_ID = "_multi_task_id";
+    public static final String MULTI_TASK_SUBJECT_ID = "_subject_id";
 
     // TEACHERS column names
     public static final String TEACHER_ID = "_id";
@@ -47,20 +44,21 @@ public class Database extends SQLiteOpenHelper {
     // SUBJECT column names
     public static final String SUBJECT_ID = "_id";
     public static final String SUBJECT_NAME = "_name";
+    public static final String SUBJECT_TEACHER_ID = "_teacher_id";
     public static final String SUBJECT_TYPE = "_type";
-
-    // TEACHER_TO_SUBJECT column names
-    public static final String TEACHER_TO_SUBJECT_TEACHER_ID = "_teacher_id";
-    public static final String TEACHER_TO_SUBJECT_SUBJECT_ID = "_subject_id";
 
     // CONTENTS column names
     public static final String CONTENT_ID = "_id";
+    public static final String CONTENT_SUBJECT_ID = "_subject_id";
+    public static final String CONTENT_TASK_ID = "_task_id";
+    public static final String CONTENT_TEST_ID = "_test_id";
 
     // CONTENT_ITEMS column names
     public static final String CONTENT_ITEM_ID = "_id";
     public static final String CONTENT_ITEM_CONTENT_ID = "_content_id";
     public static final String CONTENT_ITEM_TYPE = "_type";
-    public static final String CONTENT_SOURCE = "_source";
+    public static final String CONTENT_ITEM_SOURCE = "_source";
+    public static final String CONTENT_ITEM_TEXT = "_source";
 
     // TESTS column names
     public static final String TEST_ID = "_id";
@@ -68,53 +66,83 @@ public class Database extends SQLiteOpenHelper {
     public static final String TEST_SUBJECT_ID = "_subject_id";
     public static final String TEST_DATE = "_date";
 
-    // SUBJECT_TO_CONTENT column names
-    public static final String SUBJECT_TO_CONTENT_SUBJECT_ID = "_subject_id";
-    public static final String SUBJECT_TO_CONTENT_CONTENT_ID = "_content_id";
-
-    // TEST_TO_CONTENT column names
-    public static final String TEST_TO_CONTENT_TEST_ID = "_test_id";
-    public static final String TEST_TO_CONTENT_CONTENT_ID = "_content_id";
-
-    // TASK_TO_CONTENT column names
-    public static final String TASK_TO_CONTENT_TASK_ID = "_task_id";
-    public static final String TASK_TO_CONTENT_CONTENT_ID = "_content_id";
-
     // Table Create Statements
-    /*
-    private static final String CREATE_TABLE_SIMPLE_TASK = "create table " + SIMPLE_TASKS + "("
-            + GOAL_ID + " integer primary key autoincrement, "
-            + GOAL_NAME + " text not null, "
-            + GOAL_CATEGORY + " text not null, "
-            + GOAL_START_DATE + " text not null, "
-            + GOAL_END_DATE + " text not null, "
-            + GOAL_CURRENT_CHECK_IN_PROGRESS + " integer, "
-            + GOAL_TARGET_CHECK_IN_PROGRESS + " integer, "
-            + GOAL_CURRENT_PROGRESS + " integer, "
-            + GOAL_TARGET_PROGRESS + " integer, "
-            + GOAL_CHECK_IN_START_DATE + " text not null, "
-            + GOAL_CHECK_IN_END_DATE + " text not null, "
-            + GOAL_SPLIT_TYPE + " text not null, "
-            + GOAL_TARGET_TYPE + " text not null, "
-            KEY_ID + " INTEGER PRIMARY KEY,"
-            + KEY_TODO
-            + " TEXT," + KEY_STATUS + " INTEGER," + KEY_CREATED_AT
-            + " DATETIME" + ")";
-    */
+
+    private static final String CREATE_TABLE_SIMPLE_TASK =
+            "create table " + SIMPLE_TASKS + "("
+            + SIMPLE_TASK_ID + " integer primary key autoincrement, "
+            + SIMPLE_TASK_NAME + " text not null, "
+            + SIMPLE_TASK_SUBJECT_ID + " integer, "
+            + SIMPLE_TASK_MULTI_TASK_ID + " integer, "
+            + SIMPLE_TASK_DEADLINE + " text not null, "
+            + SIMPLE_TASK_POINTS + " integer)";
+
+    private static final String CREATE_TABLE_MULTI_TASK =
+            "create table " + MULTI_TASKS + "("
+            + MULTI_TASK_ID + " integer primary key autoincrement, "
+            + MULTI_TASK_NAME + " text not null, "
+            + MULTI_TASK_SUBJECT_ID + " integer)";
+
+    private static final String CREATE_TABLE_TEACHERS =
+            "create table " + TEACHERS + "("
+            + TEACHER_ID + " integer primary key autoincrement, "
+            + TEACHER_NAME + " text not null, "
+            + TEACHER_CONTACTS + " text)";
+
+    private static final String CREATE_TABLE_SUBJECTS =
+            "create table " + SUBJECTS + "("
+            + SUBJECT_ID + " integer primary key autoincrement, "
+            + SUBJECT_NAME + " text not null, "
+            + SUBJECT_TEACHER_ID + " integer, "
+            + SUBJECT_TYPE + " text not null)";
+
+    private static final String CREATE_TABLE_CONTENTS =
+            "create table " + CONTENTS + "("
+            + CONTENT_ID + " integer primary key autoincrement"
+            + CONTENT_SUBJECT_ID + " integer, "
+            + CONTENT_TASK_ID + " integer, "
+            + CONTENT_TEST_ID + " integer)";
+
+    private static final String CREATE_TABLE_CONTENT_ITEMS =
+            "create table " + CONTENT_ITEMS + "("
+            + CONTENT_ITEM_ID + " integer primary key autoincrement"
+            + CONTENT_ITEM_CONTENT_ID + " integer, "
+            + CONTENT_ITEM_TYPE + " text not null, "
+            + CONTENT_ITEM_SOURCE + " text, "
+            + CONTENT_ITEM_TEXT + " text)";
+
+    private static final String CREATE_TABLE_TESTS =
+            "create table " + TESTS + "("
+            + TEST_ID + " integer primary key autoincrement"
+            + TEST_TYPE + " text not null, "
+            + TEST_SUBJECT_ID + " integer, "
+            + TEST_DATE + " text not null)";
 
     public Database(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-    /*
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(DATABASE_CREATE);
+        db.execSQL(CREATE_TABLE_SIMPLE_TASK);
+        db.execSQL(CREATE_TABLE_MULTI_TASK);
+        db.execSQL(CREATE_TABLE_SUBJECTS);
+        db.execSQL(CREATE_TABLE_TEACHERS);
+        db.execSQL(CREATE_TABLE_TESTS);
+        db.execSQL(CREATE_TABLE_CONTENTS);
+        db.execSQL(CREATE_TABLE_CONTENT_ITEMS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + GOALS);
+        db.execSQL("DROP TABLE IF EXISTS " + SIMPLE_TASKS);
+        db.execSQL("DROP TABLE IF EXISTS " + MULTI_TASKS);
+        db.execSQL("DROP TABLE IF EXISTS " + SUBJECTS);
+        db.execSQL("DROP TABLE IF EXISTS " + TEACHERS);
+        db.execSQL("DROP TABLE IF EXISTS " + TESTS);
+        db.execSQL("DROP TABLE IF EXISTS " + CONTENTS);
+        db.execSQL("DROP TABLE IF EXISTS " + CONTENT_ITEMS);
+
         onCreate(db);
     }
-    */
 }
